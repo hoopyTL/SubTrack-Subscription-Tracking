3813408815bf8f0a32aff54a95ee66fa3ccb9ea1
HoopyTL <nguyenvanhop248@gmail.com>
2026-06-03 23:42:48 +0700

    feat(backend): add MongoDB connection setup

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectDB from "./database/mongodb.js";
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,8 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`SubTrack API is running on port http://localhost:${PORT}`);
+
+  await connectDB();
 });
