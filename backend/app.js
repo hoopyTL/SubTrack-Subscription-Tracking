a7d463c8695b4bc818339a4df3a001b92b0fe83a
HoopyTL <nguyenvanhop248@gmail.com>
2026-06-04 01:32:30 +0700

    feat: add error handling

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,13 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectDB from "./database/mongodb.js";
-
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the SubTrack API!");
@@ -15,6 +19,8 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`SubTrack API is running on port http://localhost:${PORT}`);
