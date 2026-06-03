efe3dc0028c86702f762abffb154e29c281fe1f7
HoopyTL <nguyenvanhop248@gmail.com>
2026-06-03 19:08:31 +0700

    feat: add auth routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the SubTrack API!");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`SubTrack API is running on port http://localhost:${PORT}`);
