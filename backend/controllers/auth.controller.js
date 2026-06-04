d376e2ddb75f7a07ada0d9a38467d30f6fd52eb0
HoopyTL <nguyenvanhop248@gmail.com>
2026-06-04 11:33:28 +0700

    feat: add sign-in auth controller with JWT

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -60,7 +60,38 @@ export const signUp = async (req, res, next) => {
 };
 
 export const signIn = async (req, res, next) => {
-  // Implement sign up logic here
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      const error = new Error("Invalid Password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User signed in successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signOut = async (req, res, next) => {
