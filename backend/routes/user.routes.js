3bd5090deb862d75b8369831811b9df550bb89e0
HoopyTL <nguyenvanhop248@gmail.com>
2026-06-04 12:51:01 +0700

    feat: add JWT auth middleware for user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getUsers, getUser } from "../controllers/user.controller.js";
+import { authorize } from "../middlewares/auth.middleware.js";
+
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
 
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => {
   res.send({ title: "CREATE new user" });
