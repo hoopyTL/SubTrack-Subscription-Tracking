69bd155bf72b0e506cd22594b632fc696eb80634
HoopyTL <nguyenvanhop248@gmail.com>
2026-06-04 11:23:37 +0700

    feat: add sign-up auth controller with JWT

--- backend/config/env.js
+++ backend/config/env.js
@@ -2,4 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, DB_URI } = process.env;
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } =
+  process.env;
---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
+
+export const signUp = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("User already exists");
+      error.statusCode = 409;
+      return next(error);
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      const newUsers = await User.create(
+        [{ name, email, password: hashedPassword }],
+        {
+          session,
+        },
+      );
+
+      const user = newUsers[0];
+
+      const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+        expiresIn: JWT_EXPIRES_IN,
+      });
+
+      await session.commitTransaction();
+      await session.endSession();
+
+      const userResponse = user.toObject();
+      delete userResponse.password;
+
+      res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        data: {
+          token,
+          user: userResponse,
+        },
+      });
+    } catch (transactionError) {
+      await session.abortTransaction();
+      await session.endSession();
+      next(transactionError);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  // Implement sign up logic here
+};
+
+export const signOut = async (req, res, next) => {
+  // Implement sign up logic here
+};
